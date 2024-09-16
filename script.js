0dcbc88f6ed134428ed7206b09364582227d049b
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-16 23:20:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 function getFormvalue() {
-      // Access the form element
-      const form = document.getElementById("form1");
+    // Get the values of the first and last name fields
+    const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
 
-      // Prevent the form from submitting normally
-      form.addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        // Get the values of the first and last name fields
-        const firstName = document.getElementById('fname').value;
-        const lastName = document.getElementById('lname').value;
-
-        // Display the result in an alert
-        alert(firstName + " " + lastName);
-      });
-    }
-
-    // Call the function to set the event listener on page load
-    window.onload = getFormvalue;
+    // Display the result in an alert
+    alert(firstName + " " + lastName);
+}
