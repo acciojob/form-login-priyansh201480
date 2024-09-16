0ce7f8cd2adef0cda3db575eec4f15b38bb7ec05
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-16 22:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
-
+    const form = document.getElementById("form1");
+	form.addEventListener('submit',function(event){
+		event.preventDefault();
+		
+	const firstName = document.getElementById('fname').value;
+    const lastName = document.getElementById('lname').value;
+    alert(firstName + " " + lastName);
+	});
 }
+window.onload = getFormvalue;
